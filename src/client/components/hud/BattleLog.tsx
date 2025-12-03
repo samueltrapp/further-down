c9646858092b7b1907802f0deb724077832af852
samueltrapp <trapp.samuel.r@gmail.com>
2025-12-02 23:59:26 -0500

    Styling battle log

--- src/client/components/hud/BattleLog.tsx
+++ src/client/components/hud/BattleLog.tsx
@@ -5,16 +5,11 @@ export default function BattleLog() {
   const game = useContext(GameContext);
   const log = game?.client?.logHistory;
 
-  console.log(log);
-
   return (
     <div className="battle-log central-column">
-      <h3 className="battle-log-header">Battle Log</h3>
-      <div style={{ display: "inline-block", verticalAlign: "bottom" }}>
-        {log?.map((logLine) => (
-          <p>{logLine}</p>
-        ))}
-      </div>
+      {log?.map((logLine) => (
+        <p>{logLine}</p>
+      ))}
     </div>
   );
 }
